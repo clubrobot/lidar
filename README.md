60f27532172488cc0e5af75826fd64f72d161e73
Pixelboris <pixelbo21@gmail.com>
2024-10-19 12:54:55 +0200

    CLion Setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lidar
 Lidar de notre robot commencé en 2025 et pour les années à venir
+
+Faire une branche de bakcup pour chaque années.
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <STM32FreeRTOS.h>
+#include <VL53L5CX.h> //TODO: je sais pas comment import voir les exemples ptdr
+
+void setup() {
+// write your initialization code here
+}
+
+void loop() {
+// write your code here
+}
