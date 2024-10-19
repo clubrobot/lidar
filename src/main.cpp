6819298e4de40457cb160bec42bf37df4b2679b5
RJRP44 <romainpouvreau@orange.fr>
2024-10-19 22:38:37 +0200

    Declare lib deps in platformio.ini

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
-#include <VL53L5CX.h> //TODO: je sais pas comment import voir les exemples ptdr
+#include <vl53l5cx_api.h>
 
 void setup() {
 // write your initialization code here
